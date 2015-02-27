96886dc46c12ad61c96e2d39849c36dd68b4a806
Anton Glukhov <toxxin@mail.ru>
2015-02-27 04:43:56 +0300

    Clean up RSSGen class

--- RSSGen.py
+++ RSSGen.py
@@ -4,10 +4,9 @@ __author__ = 'Anton Glukhov'
 __copyright__ = "Copyright 2015, Easywhere"
 __email__ = "ag@easywhere.ru"
 
-import datetime
-import time
 import calendar as cal
 import feedparser
+import logging
 
 
 class RSSGen(object):
@@ -32,21 +31,20 @@ class RSSGen(object):
         return "".join(self._html_escape_table.get(c,c) for c in text)
 
     def _make_date(self, tmp):
-        """Make timestamp, clean timezone on debug PC"""
-        if tmp.get('published') is not None:
-            date = tmp['published'][:-5] + "+0000"
-            date = datetime.datetime.strptime(date, "%a, %d %b %Y %H:%M:%S +0000")
-            return date
+        """Timestamp is output format."""
+        if tmp.get('published_parsed') is not None:
+            return cal.timegm(tmp['published_parsed'])
         elif tmp.get('updated_parsed') is not None:
             return cal.timegm(tmp['updated_parsed'])
         else:
-            print "test"
+            logging.debug("Unknown datetime field or format!")
+            return 0
 
     def _get_link(self, links):
-
-        if len(links) > 0:
-            imgs = [l for l in links if l['type'] == u'image/jpeg']
-            return imgs[0]['href'] if len(imgs) > 0 else None
+        """Attempt to grab images from feed."""
+        if links:
+            imgs = [l['href'] for l in links if l['type'] == u'image/jpeg']
+            return imgs[-1] if imgs else None
         else:
             return None
 
@@ -58,8 +56,7 @@ class RSSGen(object):
             "title": entry['title'],
             "desc": entry['summary'],
             "published": self._make_date(entry),
-            # "published": self._make_date(entry['published']) if entry['published'] is not None else self._make_date(entry['updated']),
-            # "pic": self._get_link(entry['links'])
+            "pic": self._get_link(entry['links'])
         }
 
     def generate(self):
